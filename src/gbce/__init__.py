b74eff55b314475d69a3d0ef2fd579d2e81fdae3
sreenagi456-afk <sreenagi456@gmail.com>
2025-11-09 14:49:41 +0530

    Update __init__.py

--- src/gbce/__init__.py
+++ src/gbce/__init__.py
@@ -1,1 +1,12 @@
-#init module
+""" Init module
+GBCE Super simple stock market system"
+
+__version__ = "1.0.0"
+__author__  =  "Nagi Reddy Burra"
+
+from .stock import Stock
+from .trade import Trade
+from .exchange import StockExchange
+from .enums import StockType, TradeIndicator
+
+__all__ = [ "Stock", "Trade", "StockExchange", "StockType", "TradeIndicator" ]
